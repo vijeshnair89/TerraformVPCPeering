eb6ed47ba2e7f6a232d5dd4b8d630cfc678c9a40
Vijesh Vijayan Nair <143416086+vijeshnair89@users.noreply.github.com>
2024-06-21 22:35:27 +0530

    Update main.tf

--- VPC(Userdata-to-configure)/main.tf
+++ VPC(Userdata-to-configure)/main.tf
@@ -1,6 +1,4 @@
-
-
-##VPC ap-south-1 config
+## VPC ap-south-1 configuration 
 resource "aws_vpc" "vpc01" {
   provider = aws.ap-south-1
   cidr_block = var.cidr_vpc01
@@ -9,6 +7,7 @@ resource "aws_vpc" "vpc01" {
   }
 }
 
+# Create Public Subnet
 resource "aws_subnet" "pubsub_vpc01" {
   provider = aws.ap-south-1
   vpc_id = aws_vpc.vpc01.id
@@ -18,6 +17,7 @@ resource "aws_subnet" "pubsub_vpc01" {
   }
 }
 
+# Private Subnet
 resource "aws_subnet" "prvsub_vpc01" {
   provider = aws.ap-south-1
   vpc_id = aws_vpc.vpc01.id
@@ -27,6 +27,7 @@ resource "aws_subnet" "prvsub_vpc01" {
   }
 }
 
+# Internet gateway
 resource "aws_internet_gateway" "igwvpc01" {
   provider = aws.ap-south-1
   vpc_id = aws_vpc.vpc01.id
@@ -35,6 +36,7 @@ resource "aws_internet_gateway" "igwvpc01" {
   }
 }
 
+# Create Public Route Table and attach internet gateway to route table
 resource "aws_route_table" "pubroutevpc01" {
   provider = aws.ap-south-1
   vpc_id = aws_vpc.vpc01.id
@@ -48,18 +50,21 @@ resource "aws_route_table" "pubroutevpc01" {
   }
 }
 
+
+# Associate Public Subnet to Public Route Table
 resource "aws_route_table_association" "routeassoc1" {
     provider = aws.ap-south-1
     subnet_id = aws_subnet.pubsub_vpc01.id
     route_table_id = aws_route_table.pubroutevpc01.id
-  
 }
 
+# Create Elastic IP
 resource "aws_eip" "nateipvpc01" {
   provider = aws.ap-south-1
   domain = "vpc"
 }
 
+# Create NAT Gateway
 resource "aws_nat_gateway" "natvpc01" {
   provider = aws.ap-south-1
   allocation_id = aws_eip.nateipvpc01.id
@@ -69,6 +74,7 @@ resource "aws_nat_gateway" "natvpc01" {
   }
 }
 
+# Create Private Route table and attach NAT Gateway to route
 resource "aws_route_table" "prvroutevpc01" {
   provider = aws.ap-south-1
   vpc_id = aws_vpc.vpc01.id
@@ -81,6 +87,7 @@ resource "aws_route_table" "prvroutevpc01" {
   }
 }
 
+# Associate private subnet to private route table
 resource "aws_route_table_association" "routeassoc2" {
   provider = aws.ap-south-1
   subnet_id = aws_subnet.prvsub_vpc01.id
@@ -89,8 +96,7 @@ resource "aws_route_table_association" "routeassoc2" {
 
 
 
-##VPC us-east-1  config
-
+## VPC us-east-1  configuration
 resource "aws_vpc" "vpc02" {
   provider = aws.us-east-1
   cidr_block = var.cidr_vpc02
@@ -99,6 +105,7 @@ resource "aws_vpc" "vpc02" {
   }
 }
 
+# Create Public Subnet
 resource "aws_subnet" "pubsub_vpc02" {
   provider = aws.us-east-1
   vpc_id = aws_vpc.vpc02.id
@@ -108,6 +115,7 @@ resource "aws_subnet" "pubsub_vpc02" {
   }
 }
 
+# Create Private Subnet
 resource "aws_subnet" "prvsub_vpc02" {
   provider = aws.us-east-1
   vpc_id = aws_vpc.vpc02.id
@@ -117,6 +125,7 @@ resource "aws_subnet" "prvsub_vpc02" {
   }
 }
 
+# Create Internet gateway
 resource "aws_internet_gateway" "igwvpc02" {
   provider = aws.us-east-1
   vpc_id = aws_vpc.vpc02.id
@@ -125,6 +134,7 @@ resource "aws_internet_gateway" "igwvpc02" {
   }
 }
 
+# Create Public Route table and attach internet gateway to the route table
 resource "aws_route_table" "pubroutevpc02" {
   provider = aws.us-east-1
   vpc_id = aws_vpc.vpc02.id
@@ -138,6 +148,7 @@ resource "aws_route_table" "pubroutevpc02" {
   }
 }
 
+# Associate public subnets to public route table
 resource "aws_route_table_association" "routeassoc3" {
     provider = aws.us-east-1
     subnet_id = aws_subnet.pubsub_vpc02.id
@@ -145,11 +156,13 @@ resource "aws_route_table_association" "routeassoc3" {
   
 }
 
+# Create elastic IP
 resource "aws_eip" "nateipvpc02" {
   provider = aws.us-east-1
   domain = "vpc"
 }
 
+# Create NAT Gateway
 resource "aws_nat_gateway" "natvpc02" {
   provider = aws.us-east-1
   allocation_id = aws_eip.nateipvpc02.id
@@ -159,6 +172,7 @@ resource "aws_nat_gateway" "natvpc02" {
   }
 }
 
+# Create Private Route Table and attach NAT gateway to route
 resource "aws_route_table" "prvroutevpc02" {
   provider = aws.us-east-1
   vpc_id = aws_vpc.vpc02.id
@@ -171,6 +185,7 @@ resource "aws_route_table" "prvroutevpc02" {
   }
 }
 
+# Associate private subnet to private route table
 resource "aws_route_table_association" "routeassoc4" {
   provider = aws.us-east-1
   subnet_id = aws_subnet.prvsub_vpc02.id
@@ -179,7 +194,6 @@ resource "aws_route_table_association" "routeassoc4" {
 
 
 ## VPC Peering between ap-south-1 and us-east-1
-
 resource "aws_vpc_peering_connection" "peer" {
     provider = aws.ap-south-1
     vpc_id = aws_vpc.vpc01.id
@@ -204,7 +218,6 @@ resource "aws_vpc_peering_connection_accepter" "peer_accept" {
 }
 
 ## Add the vpc peering to the route tables
-
 resource "aws_route" "r1" {
   provider = aws.ap-south-1
   route_table_id = aws_route_table.pubroutevpc01.id
@@ -233,7 +246,7 @@ resource "aws_route" "r4" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
-#Generate keypair for instances
+#Generate keypair for instances in ap-south-1 and us-east-1
 resource "aws_key_pair" "keypairmum" {
   provider = aws.ap-south-1
   key_name = "terraform-key"
@@ -246,7 +259,7 @@ resource "aws_key_pair" "keypairus" {
   public_key = file("C:/Users/Vijesh/.ssh/id_rsa.pub")
 }
 
-# Security group vpc1
+# Security group vpc1 (ap-south-1)
 resource "aws_security_group" "sgvpc01" {
   provider = aws.ap-south-1
   name = "web"
@@ -287,7 +300,7 @@ resource "aws_security_group" "sgvpc01" {
   }
 }
 
-# Security groups vpc2
+# Security groups vpc2 (us-east-1)
 resource "aws_security_group" "sgvpc02" {
   provider = aws.us-east-1
   name = "sgvpc02"
@@ -329,7 +342,7 @@ resource "aws_security_group" "sgvpc02" {
 }
 
 ## Creating instances 1 public bastion and  2 private instances in each private subnets 
-resource "aws_instance" "instance1vpc01" {
+resource "aws_instance" "instance1vpc02" {
   provider = aws.us-east-1
   ami = var.us-east-ami
   instance_type = var.us-east-instance
@@ -339,12 +352,11 @@ resource "aws_instance" "instance1vpc01" {
   associate_public_ip_address = true
   user_data = base64encode(file("configure.sh"))
   tags = {
-    Name = "instance1vpc01"
+    Name = "instance1vpc02"
   }
 }
 
-
-resource "aws_instance" "instance2vpc01" {
+resource "aws_instance" "instance2vpc02" {
   provider = aws.us-east-1
   ami = var.us-east-ami
   instance_type = var.us-east-instance
@@ -354,11 +366,11 @@ resource "aws_instance" "instance2vpc01" {
   user_data = base64encode(file("configure.sh"))
 
   tags = {
-    Name = "instance2vpc01"
+    Name = "instance2vpc02"
   }
 }
 
-resource "aws_instance" "instance1vpc02" {
+resource "aws_instance" "instance1vpc01" {
   provider = aws.ap-south-1
   ami = var.ap-south-ami
   instance_type = var.ap-south-instance
@@ -367,6 +379,6 @@ resource "aws_instance" "instance1vpc02" {
   vpc_security_group_ids = [aws_security_group.sgvpc01.id]
   user_data = base64encode(file("configure.sh"))
   tags = {
-    Name = "instance1vpc02"
+    Name = "instance1vpc01"
   }
 }
